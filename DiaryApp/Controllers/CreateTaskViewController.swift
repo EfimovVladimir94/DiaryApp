e140a9140148c92a2a086db5d81c5297924f457a
Efimov <efimov@mail.ru>
2021-01-24 16:08:28 +0300

    added image picker

--- DiaryApp/Controllers/CreateTaskViewController.swift
+++ DiaryApp/Controllers/CreateTaskViewController.swift
@@ -98,7 +98,30 @@ class CreateTaskViewController: UIViewController {
     
     
     @objc private func onTapped() {
-        print("pressed")
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        // не проверял работоспособность из за отсутствия реального устройства
+        let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+            self.chooseImagePicker(source: .camera)
+        }
+        let photo = UIAlertAction(title: "Photo", style: .default) { _ in
+            self.chooseImagePicker(source: .photoLibrary)
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+        actionSheet.addAction(camera)
+        actionSheet.addAction(photo)
+        actionSheet.addAction(cancel)
+        present(actionSheet, animated: true)
     }
 
+}
+
+extension CreateTaskViewController {
+    func chooseImagePicker(source: UIImagePickerController.SourceType) {
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true)
+        }
+    }
 }
